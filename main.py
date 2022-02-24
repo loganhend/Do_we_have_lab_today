bf5cc0bdb70033778d7b23f84763e7034adf5d2b
Your NameWetGrapes-mat <matvey.diubuilo@icloud.com>
2022-02-24 23:45:11 +0300

    instruction

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 if __name__ == '__main__':
     print('Привет')
-    print('Добаьте в интерпритатор модуль Kivy')
+    print('Добаьте в интерпритатор модуль kivy')
+    print('Нижний правый угл')
+    print('interpreter settings')
+    print('Python interpreter ')
+    print('Нажимаем на плюсик')
+    print('ищем kyvi устанавливаем ')
+
+
 
